3acc3d34ce0b1e5d9e732e9d4b9b29010dbef8f7
swarup Goswami <goswamiswarup97@gmail.com>
2025-07-21 01:26:24 +0530

    updated the createRepo tool

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,14 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
+import { createRepo } from "./tools/createRepo.js"
+ // very top of server.ts
+import {z } from 'zod';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config(); // Load environment variables from .env file
+
 
 const server =new McpServer({
     name:"github-mcp",
@@ -11,9 +20,33 @@ const server =new McpServer({
     }
 })
 
-server.tool("createRepo","creates a repo in the github",{
-  
-})
+const inputSchema = z.object({
+  owner: z.string(),
+  repo: z.string(),
+});
+
+server.tool(
+  "createRepo",
+  {
+    name: z.string().describe("name of the repo"),
+    description: z.string().optional(),
+    private: z.boolean().optional().default(true),
+  },
+  async (
+    input: { name: string; description?: string; private?: boolean },
+    extra
+  ) => {
+    const result = await createRepo(input);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `Repository created: ${result.message}\nURL: ${result.url}`,
+        },
+      ],
+    };
+  }
+);
 
 async function main(){
   const transport =new StdioServerTransport()
