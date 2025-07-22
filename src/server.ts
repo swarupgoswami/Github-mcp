fd1b14fa768cfb41467953f7fd3e90eaa026aed0
swarup Goswami <goswamiswarup97@gmail.com>
2025-07-23 01:23:54 +0530

    updated server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,21 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js"
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
-import { createRepo } from "./tools/createRepo.js"
- // very top of server.ts
-import {z } from 'zod';
-import dotenv from 'dotenv';
-
-
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { createRepo } from "./tools/createRepo.js";
+// very top of server.ts
+import { z } from "zod";
+import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables from .env file
 
-
-const server =new McpServer({
-    name:"github-mcp",
-    version:"1.0.0",
-    capabilities:{
-        resources:{},
-        tools:{},
-        prompts:{},
-    }
-})
+const server = new McpServer({
+  name: "github-mcp",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+    prompts: {},
+  },
+});
 
 const inputSchema = z.object({
   owner: z.string(),
@@ -27,16 +24,27 @@ const inputSchema = z.object({
 
 server.tool(
   "createRepo",
+  "creates a new repository on GitHub",
   {
     name: z.string().describe("name of the repo"),
     description: z.string().optional(),
     private: z.boolean().optional().default(true),
   },
+  {
+    title: "Create Repository",
+    readOnlyHint: false,
+    destructiveHint: false,
+    idempotentHint: false,
+    openWorldHint: true,
+  },
   async (
     input: { name: string; description?: string; private?: boolean },
     extra
   ) => {
+    console.log("Input received for createRepo:", input);
     const result = await createRepo(input);
+    console.log("createRepo called with:", input.name);
+
     return {
       content: [
         {
@@ -48,10 +56,9 @@ server.tool(
   }
 );
 
-async function main(){
-  const transport =new StdioServerTransport()
+async function main() {
+  const transport = new StdioServerTransport();
   await server.connect(transport);
 }
 
-
-main()
+main();
