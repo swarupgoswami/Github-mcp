b34bddda31bd43d331489532f3b922d75937f711
swarup Goswami <goswamiswarup97@gmail.com>
2025-07-26 21:12:53 +0530

    added more tools

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { createRepo } from "./tools/createRepo.js";
+import { createRepo  } from "./tools/createRepo.js";
+import {getIssues} from "./tools/getIssues.js"
+import { listRepos } from "./tools/listRepos.js";
 // very top of server.ts
 import { z } from "zod";
 import dotenv from "dotenv";
@@ -55,6 +57,94 @@ server.tool(
     };
   }
 );
+
+server.tool("getIssues","get the open issues from a github repository",
+  {
+    owner:z.string().describe("owner of the github repository"),
+    repo:z.string().describe("name of the fithub repository"),
+  },
+  {
+    title:"get repository issues",
+    readOnlyHint:true,
+    destructiveHint:false,
+    idempotentHint:true,
+    openWorldHint:true,
+  },
+  async(
+    input:{owner:string;repo:string},extra)=>{
+
+    console.log('input recived for getIssues',input);
+    const issues=await getIssues(input);
+    console.log("issues fetched",JSON.stringify(issues));
+
+    if(issues.length === 0)
+    {
+      return{
+        content:[
+          {
+            type:'text',
+            text:`no open issues found in the given repo ${input.owner}/${input.repo} `
+          }
+        ]
+      }
+    }
+
+
+    const issuesText= issues.map(
+      (issue) => `#${issue.number} - ${issue.title}\n${issue.url}`
+    ).join('\n\n');
+
+    return{
+      content:[
+        {
+          type:"text",
+          text:`open issues in /${input.owner}/${input.repo}:\n\n${issuesText} `
+        }
+      ]
+    }
+  }
+)
+
+server.tool("listRepo",
+  "list all the repo of a github user",
+  {
+    username:z.string().describe("github username to list repositories for")
+  },
+  {
+    title:"list repositories",
+    readOnlyHint:true,
+    destructiveHint:false,
+    idempotentHint:false,
+    openWorldHint:true,
+  },
+  async(
+    input:{username:string},extra
+  )=>{
+    console.log("input revived",input);
+    const repos=await listRepos(input);
+    console.log("repositories fetched",JSON.stringify(repos));
+
+    if(repos.length === 0){
+      return{
+        content:[{
+          type:"text",
+          text:"no repositories found for user "}
+        ]
+      }
+    }
+
+    const reposText=repos.map((repo)=>`$(repo.name) - ${repo.url}`).join('\n\n');
+    return{
+      content:[
+        {
+          type:"text",
+          text:`repo for ${input.username}:\n\n${reposText}`,
+        },
+      ],
+    };
+  }
+
+)
 
 async function main() {
   const transport = new StdioServerTransport();
